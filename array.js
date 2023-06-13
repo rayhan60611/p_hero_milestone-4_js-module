ff3262aa40d8d42adc7dddeed1fd5490070c3b88
rayhan60611 <pecon.alok@gmail.com>
2023-06-13 12:41:21 +0600

    array index find

--- array.js
+++ array.js
@@ -1,1 +1,10 @@
-console.log(17);
+// Programming Hero Module 18 Arrays and Conditions//
+
+var myArray = [1, 2, 3, 4];
+
+console.log(myArray.length);
+
+//find index of an array element
+
+var positionIndex = myArray.indexOf(3);
+console.log(positionIndex);
